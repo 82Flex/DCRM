2c031189999868461c0fcfc709a6f622ca35da0b
i_82 <5410705+Lessica@users.noreply.github.com>
2019-11-29 22:50:09 +0800

    Update uwsgi-run.sh

--- uwsgi-run.sh
+++ uwsgi-run.sh
@@ -5,10 +5,10 @@ if [ "${DCRM_WORKER}" = "none" ]; then
     python manage.py migrate --no-input
     uwsgi --ini uwsgi.ini
 elif [ "${DCRM_WORKER}" = "default" ]; then
-    python manage.py migrate --no-input
+    # python manage.py migrate --no-input
     python manage.py rqworker default
 elif [ "${DCRM_WORKER}" = "high" ]; then
-    python manage.py migrate --no-input
+    # python manage.py migrate --no-input
     python manage.py rqworker high
 fi
 
