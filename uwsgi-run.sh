41612d64acf292af19f842c5973377681bcb7836
i_82 <5410705+Lessica@users.noreply.github.com>
2019-12-01 01:03:48 +0800

    Update uwsgi-run.sh

--- uwsgi-run.sh
+++ uwsgi-run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 if [ "${DCRM_WORKER}" = "none" ]; then
+    sleep 15  # wait for db to be ready
     python manage.py collectstatic --no-input
     python manage.py migrate --no-input
     uwsgi --ini uwsgi.ini
 elif [ "${DCRM_WORKER}" = "default" ]; then
-    sleep 60  # wait for main app to launch
+    sleep 30  # wait for main app to launch
     python manage.py rqworker default
 elif [ "${DCRM_WORKER}" = "high" ]; then
-    sleep 60  # wait for main app to launch
+    sleep 30  # wait for main app to launch
     python manage.py rqworker high
 fi
 
